3b0aaeb61b1df4ed42e2057ce7bb9c43a00293f4
liuguili <gongzili456@gmail.com>
2015-07-29 14:52:15 +0800

    ucpaas 0.1

---
+++ test/sms.js
@@ -0,0 +1,25 @@
+"use strict";
+var co = require('co');
+
+var UCPaas = require('../lib/ucpaas');
+
+co(function* () {
+
+
+  var ucpaas = UCPaas({
+    accountSid: 'accountSid',
+    token: 'token',
+    appId: 'appId'
+  });
+
+  var result = yield ucpaas.sms({
+    "param": "AppName,1024,1",
+    "templateId": "9947",
+    "to": "18512345678"
+  });
+
+  console.log('result: ', result);
+});
+
+
+
